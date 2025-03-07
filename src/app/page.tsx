3f9817554403cc082975d91d4bceb5624dc7d8c3
Zaeem Altaf <official.zaeemaltaf@gmail.com>
2025-03-07 17:10:04 +0500

    fix build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,22 +4,13 @@ import About from "@/components/About";
 import HeroSection from "@/components/Hero";
 import LogoLoader from "@/components/logoLoader";
 import Skills from "@/components/Skills";
-import locomotiveScroll from "locomotive-scroll";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 
 
 export default function Home() {
-  const scrollRef = useRef<HTMLDivElement>(null);
   const [loading, setLoading] = useState(true)
 
-  useEffect(() => {
-    if (typeof window !== 'undefined' && scrollRef.current) {
-      const scroll = new locomotiveScroll({
-        el: scrollRef.current,
-        smooth: true
-      });
-    }
-  }, []);
+  
 
   useEffect(() => {
     setTimeout(() => {
