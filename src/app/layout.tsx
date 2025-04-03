8c53c0d2dfcd7f90e4b70663e91b1919905b5cfc
Zaeem Altaf <official.zaeemaltaf@gmail.com>
2025-04-03 19:03:17 +0500

    add meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-site-verification" content="9Jm-lCyk2ZAGml9QWXLCkgwkzrb78gbR7Pbw4g5BTEw" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
